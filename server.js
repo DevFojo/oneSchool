94122b2389c72487b88eab70be60924215c42e3b
Femi Ojo <femiojo24@gmail.com>
2017-10-15 05:25:29 +0100

    extended routes

--- server.js
+++ server.js
@@ -39,26 +39,76 @@ function handleError(res, reason, message, code) {
 
 /*  "/api/students"
  *    GET: finds all students
- *    POST: creates a new contact
+ *    POST: creates a new student
  */
 
 app.get("/api/students", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get students.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post("/api/students", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new student.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
 });
 
 /*  "/api/students/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
+ *    GET: find student by id
+ *    PUT: update student by id
+ *    DELETE: deletes student by id
  */
 
 app.get("/api/students/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get student");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/api/students/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update student");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
 });
 
 app.delete("/api/students/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete student");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
 });
