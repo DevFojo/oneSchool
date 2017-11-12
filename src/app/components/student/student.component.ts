ec9185d8e51bbc9cea3bc97abdac24fb78101ff0
Femi Ojo <femiojo24@gmail.com>
2017-11-12 08:19:01 +0100

    .

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../services/student.service';
 import { FacultyService } from '../../services/faculty.service';
 import { DepartmentService } from '../../services/department.service';
+import { stagger } from '@angular/core/src/animation/dsl';
 declare var jquery: any;
 declare var $: any;
 
@@ -29,7 +30,6 @@ export class StudentComponent implements OnInit {
     this.refreshFacultyList();
     this.refreshDepartmentList();
     $('.ui.dropdown').dropdown();
-    this.validationInit();
   }
 
   refreshStudentList() {
@@ -46,23 +46,36 @@ export class StudentComponent implements OnInit {
   onFacultySelected() {
     var selectedFaculty = $('#faculty').val();
     $('#department').val('Select a Department').change();
-    this.departments = this.departmentList.filter(d => d.faculty == selectedFaculty);
+    this.filterDepartment(selectedFaculty)
+  }
+  filterDepartment(faculty: string) {
+    this.departments = this.departmentList.filter(d => d.faculty == faculty);
+  }
+  onEditFacultySelected() {
+    var selectedFaculty = $('#editStudent-faculty').val();
+    $('#editStudent-department').val('Select a Department').change();
+    this.filterDepartment(selectedFaculty);
   }
 
   loadAddStudentModal() {
+    this.addFormValidationInit()
     $('#addStudentForm').modal('show');
   }
 
   loadEditStudentModal(student: any) {
+    this.editFormValidationInit();
+    this.filterDepartment(student.faculty);
     console.log(student);
     this.newStudent = student;
     $('#editStudent-gender').val(student.gender).change();
+    $('#editStudent-faculty').val(student.faculty).change();
+    $('#editStudent-department').val(student.department).change();
+    $('#editStudent-level').val(student.level).change();
     $('#editStudentForm').modal('show');
   }
 
   loadStudentDetailsModal(student: any) {
     console.log(student);
-    this.newStudent = student;
     $('#studentDetails-name').html(student.firstName + ' ' + student.lastName);
     $('#studentDetails-matricNo').html(student.matricNo);
     $('#studentDetails-firstName').html(student.firstName);
@@ -87,7 +100,7 @@ export class StudentComponent implements OnInit {
     }
   }
   add() {
-    if ($('#addStudentForm').form('is valid')) {
+    if ($('#addForm').form('is valid')) {
       console.log(this.newStudent);
       this._studentService.create(this.newStudent).subscribe(res => {
         console.log(res);
@@ -114,15 +127,16 @@ export class StudentComponent implements OnInit {
     this.studentModelInit();
   }
   edit() {
-    this._studentService.update(this.newStudent).subscribe(res => {
-      console.log(res);
-    });
-    $('#editStudentForm').modal('hide');
-    this.studentModelInit();
+    if ($('#editForm').form('is valid')) {
+      this._studentService.update(this.newStudent).subscribe(res => {
+        console.log(res);
+      });
+      $('#editStudentForm').modal('hide');
+      this.studentModelInit();
+    }
   }
-
-  validationInit(): any {
-    $('.ui.form')
+  addFormValidationInit(): any {
+    $('#addForm')
       .form({
         fields: {
           firstName: {
@@ -221,6 +235,111 @@ export class StudentComponent implements OnInit {
         inline: true
       })
       ;
-  }
+  };
+  editFormValidationInit(): any {
+    $('#editForm')
+      .form({
+        fields: {
+          firstName: {
+            identifier: 'firstName',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter your first name'
+              }
+            ]
+          },
+          lastName: {
+            identifier: 'lastName',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter your last name'
+              }
+            ]
+          },
+          gender: {
+            identifier: 'gender',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please select a gender'
+              },
+              {
+                type: 'not[default]',
+                prompt: 'Please select a gender'
+              }
+            ]
+          },
+          dob: {
+            identifier: 'dob',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please select a birth date'
+              }
+            ]
+          },
+          matricNo: {
+            identifier: 'matricNo',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter a matric number'
+              },
+              {
+                type: 'minLength[6]',
+                prompt: 'Matric number must be at least {ruleValue} characters'
+              },
+              {
+                type:'number',
+                prompt:'Matric number can only contain numeric values'
+              }
+            ]
+          },
+          faculty: {
+            identifier: 'faculty',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please select a faculty'
+              },
+              {
+                type: 'not[default]',
+                prompt: 'Please select a faculty'
+              }
+            ]
+          },
+          department: {
+            identifier: 'department',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please select a department'
+              },
+              {
+                type: 'not[default]',
+                prompt: 'Please select a department'
+              }
+            ]
+          },
+          level: {
+            identifier: 'level',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please select a level'
+              },
+              {
+                type: 'not[default]',
+                prompt: 'Please select a level'
+              }
+            ]
+          }
+        },
+        inline: true
+      })
+      ;
+  };
 
 }
