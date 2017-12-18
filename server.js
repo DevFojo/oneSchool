2d4c751afe5cffd9fa01c0b933fea47f7aa7585c
Femi Ojo <femiojo24@gmail.com>
2017-12-18 02:09:25 +0100

    ...

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-
+var morgan = require('morgan');
 // Get our API routes
 const api = require('./server/routes/api');
 
 const app = express();
-
+app.use(morgan('dev'));
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
